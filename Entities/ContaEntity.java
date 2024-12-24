4508c5352fab73745c0ce0d2c65271ca2cf0cee5
layonCarvalho <layon.300@hotmail.com>
2024-12-24 15:30:59 -0300

    ContaEntity

---
+++ Entities/ContaEntity.java
@@ -0,0 +1,40 @@
+package Entities;
+
+public class ContaEntity {
+
+    private Long id;
+    private String titular;
+    private Double saldo;
+
+    public ContaEntity() {}
+
+    public ContaEntity(Long id, String titular, Double saldo) {
+        this.id = id;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
